710eb18c26160641d1c2cc4ac796a1c626f9076a
murilodpl <inayievmpl@gmail.com>
2022-02-10 12:46:20 -0300

    Update Start.jsx - Add classes - Change txt

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,9 +1,9 @@
 export default function Start(props) {
     return (
-        <div>
+        <div className="startScreen">
             <h1>Quizzical</h1>
-            <p>Some description if needed</p>
-            <button onClick={props.startGame}>Start quiz</button>
+            <p>I'm a puzzle of different topics</p>
+            <button className="btn-quiz" onClick={props.startGame}>Start quiz</button>
         </div>
     )
 }
