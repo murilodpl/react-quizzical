ed872bc3735f34b54fe8bd0c930772749a70af9a
Murilo <inayievmpl@gmail.com>
2022-02-11 21:44:44 -0300

    Update Start.jsx
    - Add two selects

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -3,6 +3,52 @@ export default function Start(props) {
         <div className="startScreen">
             <h1>Quizzical</h1>
             <p>I'm a puzzle of different topics</p>
+
+            <div className="selectDiv">
+                <select
+                    id="category"
+                    value={props.select.category}
+                    onChange={props.handleChange}
+                    name="category">
+                    <option value="0">Any Category</option>
+                    <option value="9">General Knowledge</option>
+                    <option value="10">Entertainment: Books</option>
+                    <option value="11">Entertainment: Film</option>
+                    <option value="12">Entertainment: Music</option>
+                    <option value="13">Entertainment: Musicals &amp; Theatres</option>
+                    <option value="14">Entertainment: Television</option>
+                    <option value="15">Entertainment: Video Games</option>
+                    <option value="16">Entertainment: Board Games</option>
+                    <option value="17">Science &amp; Nature</option>
+                    <option value="18">Science: Computers</option>
+                    <option value="19">Science: Mathematics</option>
+                    <option value="20">Mythology</option>
+                    <option value="21">Sports</option>
+                    <option value="22">Geography</option>
+                    <option value="23">History</option>
+                    <option value="24">Politics</option>
+                    <option value="25">Art</option>
+                    <option value="26">Celebrities</option>
+                    <option value="27">Animals</option>
+                    <option value="28">Vehicles</option>
+                    <option value="29">Entertainment: Comics</option>
+                    <option value="30">Science: Gadgets</option>
+                    <option value="31">Entertainment: Japanese Anime &amp; Manga</option>
+                    <option value="32">Entertainment: Cartoon &amp; Animations</option>
+                </select>
+
+                <select
+                    id="difficulty"
+                    value={props.select.difficulty}
+                    onChange={props.handleChange}
+                    name="difficulty">
+                    <option value="0">Any Difficulty</option>
+                    <option value="easy">Easy</option>
+                    <option value="medium">Medium</option>
+                    <option value="hard">Hard</option>
+                </select>
+            </div>
+
             <button className="btn-quiz" onClick={props.startGame}>Start quiz</button>
         </div>
     )
